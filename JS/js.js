e761db56fc4695ee7745fbe13ffead0671686b50
elvirabareeva <elvira.bareeva@hotmail.com>
2021-01-12 20:02:42 +0300

    fff

--- JS/js.js
+++ JS/js.js
@@ -1,21 +1,4 @@
-let selector = document.querySelectorAll('input[type="tel"]');
-let im = new Inputmask('+7 (999) 999-99-99');
-im.mask(selector);
 
-let selector2 = document.querySelector('input[type="tel"]');
-
-selector2.addEventListener('input', function(){
-	console.log(selector2.value)
-
-
-	const re = /^\d*(\.\d+)?$/
-
-	console.log(selector2.value.match(/[0-9]/g).length)
-
-
-	console.log(selector2.value.replace(/[0-9]/g, "0"));
-	
-});
 
 const fileInput = document.querySelector('input[type="file"]');
 
